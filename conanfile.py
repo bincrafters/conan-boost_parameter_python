76622bdd65ef547ca9cedf54106141eb9a64298e
Rene Rivera <grafikrobot@gmail.com>
2018-12-17 12:45:15 -0600

    Initial 1.69.0. [skip ci]

---
+++ conanfile.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import python_requires
+
+base = python_requires("boost_base/1.69.0@bincrafters/testing")
+
+
+class BoostParameterPythonConan(base.BoostBaseConan):
+    name = "boost_parameter_python"
+    url = "https://github.com/bincrafters/conan-boost_parameter_python"
+    lib_short_names = ["parameter_python"]
+    header_only_libs = ["parameter_python"]
+    b2_requires = [
+        "boost_mpl",
+        "boost_parameter",
+        "boost_preprocessor",
+        "boost_python"
+    ]
+
+    def package_info_additional(self):
+        self.info.options["boost_python"].python_version = "any"
